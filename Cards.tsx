bcc16809beffb8836ad0c3a55dccef020a12e1e8
Wlf04 <119137810+Wlf04@users.noreply.github.com>
2024-06-30 07:02:47 -0600

    Add files via upload

---
+++ Cards.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+const Cards = () => (
+  <>
+   
+    <style >{`
+      .cards {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 1rem;
+        margin: 0 auto;
+        margin-top: 4rem;
+        max-width: 70rem;
+      }
+    `}</style>
+  </>
+);
+
+export default Cards;
